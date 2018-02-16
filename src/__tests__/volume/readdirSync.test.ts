9111c046197b7752673a31cbb98a3729ad40aa95
Vadim Dalecky <streamich@gmail.com>
2018-02-16 20:48:48 +0000

    test: add readdirSync tests

---
+++ src/__tests__/volume/readdirSync.test.ts
@@ -0,0 +1,42 @@
+import {create} from "../util";
+
+describe('readdirSync()', () => {
+  it('returns a single directory', () => {
+    const vol = create({
+      '/foo/bar': 'baz'
+    });
+    const dirs = vol.readdirSync('/');
+
+    expect(dirs).toEqual(['foo']);
+  });
+
+  it('returns multiple directories', () => {
+    const vol = create({
+      '/foo/bar': 'baz',
+      '/tro/lo': 'lo',
+      '/ab/ra': 'kadabra',
+    });
+    const dirs = vol.readdirSync('/');
+
+    dirs.sort();
+
+    expect(dirs).toEqual(['ab', 'foo', 'tro']);
+  });
+
+  it('returns empty array when dir empty', () => {
+    const vol = create({});
+    const dirs = vol.readdirSync('/');
+
+    expect(dirs).toEqual([]);
+  });
+
+  it('respects symlinks', () => {
+    const vol = create({});
+
+    vol.symlinkSync('/', '/foo');
+
+    const dirs = vol.readdirSync('/foo');
+
+    expect(dirs).toEqual([]);
+  });
+});

886b5fd5cd98b2382c816f4a095eaa0c012dbf7d
Vadim Dalecky <streamich@gmail.com>
2018-05-12 14:08:34 +0100

    feat: implement flags for copyFileSync

--- src/__tests__/volume/copyFileSync.test.ts
+++ src/__tests__/volume/copyFileSync.test.ts
@@ -1,4 +1,5 @@
 import {create} from "../util";
+import {constants} from '../../constants';
 
 describe('copyFileSync(src, dest[, flags])', () => {
     it('method exists', () => {
@@ -35,5 +36,59 @@ describe('copyFileSync(src, dest[, flags])', () => {
         vol.copyFileSync('/foo', '/bar');
 
         expect(vol.readFileSync('/bar', 'utf8')).toBe('hello world');
+    });
+
+    describe('when COPYFILE_EXCL flag set', () => {
+        it('should copy file, if destination does not exit', () => {
+            const vol = create({
+                '/foo': 'hello world',
+            });
+
+            vol.copyFileSync('/foo', '/bar', constants.COPYFILE_EXCL);
+
+            expect(vol.readFileSync('/foo', 'utf8')).toBe('hello world');
+            expect(vol.readFileSync('/bar', 'utf8')).toBe('hello world');
+        });
+
+        it('should throw, if file already exists', () => {
+            const vol = create({
+                '/foo': 'hello world',
+                '/bar': 'no hello',
+            });
+
+            expect(() => {
+                vol.copyFileSync('/foo', '/bar', constants.COPYFILE_EXCL);
+            }).toThrowError(/EEXIST/);
+
+            expect(vol.readFileSync('/foo', 'utf8')).toBe('hello world');
+            expect(vol.readFileSync('/bar', 'utf8')).toBe('no hello');
+        });
+    });
+
+    describe('when COPYFILE_FICLONE flag set', () => {
+        it('copies file', () => {
+            const vol = create({
+                '/foo': 'hello world',
+            });
+
+            vol.copyFileSync('/foo', '/bar', constants.COPYFILE_FICLONE);
+
+            expect(vol.readFileSync('/foo', 'utf8')).toBe('hello world');
+            expect(vol.readFileSync('/bar', 'utf8')).toBe('hello world');
+        });
+    });
+
+    describe('when COPYFILE_FICLONE_FORCE flag set', () => {
+        it('always fails with ENOSYS', () => {
+            const vol = create({
+                '/foo': 'hello world',
+            });
+
+            expect(() => {
+                vol.copyFileSync('/foo', '/bar', constants.COPYFILE_FICLONE_FORCE);
+            }).toThrowError(/ENOSYS/);
+
+            expect(vol.readFileSync('/foo', 'utf8')).toBe('hello world');
+        });
     });
 });

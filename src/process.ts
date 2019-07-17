acd0f56ea5f1ecd54ba10d1493425b5c09225de5
Gareth Jones <jones258@gmail.com>
2019-07-17 16:00:03 +1200

    refactor: `createProcess` fn to be compatible w/ `strictNullChecks`

--- src/process.ts
+++ src/process.ts
@@ -2,8 +2,11 @@
 
 export interface IProcess {
   getuid(): number;
+
   getgid(): number;
+
   cwd(): string;
+
   platform: string;
   nextTick: (callback: (...args) => void, ...args) => void;
   emitWarning: (message: string, type: string) => void;
@@ -12,14 +15,30 @@ export interface IProcess {
   };
 }
 
-export function createProcess(): IProcess {
-  let p: IProcess = typeof process !== 'undefined' && process;
-  if (!p) {
-    try {
-      p = require('process');
-    } catch (e) {}
+/**
+ * Looks to return a `process` object, if one is available.
+ *
+ * The global `process` is returned if defined;
+ * otherwise `require('process')` is attempted.
+ *
+ * If that fails, `undefined` is returned.
+ *
+ * @return {IProcess | undefined}
+ */
+const maybeReturnProcess = (): IProcess | undefined => {
+  if (typeof process !== 'undefined') {
+    return process;
   }
-  if (!p) p = {} as IProcess;
+
+  try {
+    return require('process');
+  } catch {
+    return undefined;
+  }
+};
+
+export function createProcess(): IProcess {
+  const p: IProcess = maybeReturnProcess() || ({} as IProcess);
 
   if (!p.getuid) p.getuid = () => 0;
   if (!p.getgid) p.getgid = () => 0;

3edcac1c992c3bd7edaca793024c50934dbc028d
Vadim Dalecky <streamich@gmail.com>
2023-06-15 01:10:43 +0200

    feat: 🎸 improve writing to file

--- src/node-to-fsa/__tests__/NodeFileSystemFileHandle.test.ts
+++ src/node-to-fsa/__tests__/NodeFileSystemFileHandle.test.ts
@@ -26,6 +26,53 @@ maybe('NodeFileSystemFileHandle', () => {
   });
 
   describe('.createWritable()', () => {
+    describe('.truncate()', () => {
+      test('can truncate file', async () => {
+        const { dir, fs } = setup({
+          'file.txt': '012345',
+        });
+        const entry = await dir.getFileHandle('file.txt');
+        const writable = await entry.createWritable({keepExistingData: true});
+        await writable.truncate(3);
+        await writable.close();
+        expect(fs.readFileSync('/file.txt', 'utf8')).toBe('012');
+      });
+
+      test('can truncate file - 2', async () => {
+        const { dir, fs } = setup({
+          'file.txt': '012345',
+        });
+        const entry = await dir.getFileHandle('file.txt');
+        const writable = await entry.createWritable({keepExistingData: true});
+        await writable.write({type: 'truncate', size: 3})
+        await writable.close();
+        expect(fs.readFileSync('/file.txt', 'utf8')).toBe('012');
+      });
+
+      test('can truncate up', async () => {
+        const { dir, fs } = setup({
+          'file.txt': '012345',
+        });
+        const entry = await dir.getFileHandle('file.txt');
+        const writable = await entry.createWritable({keepExistingData: true});
+        await writable.write({type: 'truncate', size: 10})
+        await writable.close();
+        expect(fs.readFileSync('/file.txt').length).toBe(10);
+        expect(fs.readFileSync('/file.txt')[8]).toBe(0);
+      });
+
+      test('on up truncation bytes are nulled', async () => {
+        const { dir, fs } = setup({
+          'file.txt': '012345',
+        });
+        const entry = await dir.getFileHandle('file.txt');
+        const writable = await entry.createWritable({keepExistingData: true});
+        await writable.write({type: 'truncate', size: 10})
+        await writable.close();
+        expect(fs.readFileSync('/file.txt')[8]).toBe(0);
+      });
+    });
+
     describe('.write(chunk)', () => {
       test('overwrites the file when write is being executed', async () => {
         const { dir, fs } = setup({
@@ -38,6 +85,17 @@ maybe('NodeFileSystemFileHandle', () => {
         expect(fs.readFileSync('/file.txt', 'utf8')).toBe('...');
       });
 
+      test('writes at file start', async () => {
+        const { dir, fs } = setup({
+          'file.txt': '...',
+        });
+        const entry = await dir.getFileHandle('file.txt');
+        const writable = await entry.createWritable({keepExistingData: true});
+        await writable.write('1');
+        await writable.close();
+        expect(fs.readFileSync('/file.txt', 'utf8')).toBe('1..');
+      });
+
       test('can seek and then write', async () => {
         const { dir, fs } = setup({
           'file.txt': '...',
@@ -47,8 +105,35 @@ maybe('NodeFileSystemFileHandle', () => {
         writable.seek(1);
         await writable.write('1');
         await writable.write('2');
-        await writable.close();
         expect(fs.readFileSync('/file.txt', 'utf8')).toBe('.12');
+        writable.seek(0);
+        await writable.write('0');
+        await writable.close();
+        expect(fs.readFileSync('/file.txt', 'utf8')).toBe('012');
+      });
+    });
+
+    describe('.write(options)', () => {
+      test('can write at offset, when providing position in write call', async () => {
+        const { dir, fs } = setup({
+          'file.txt': '...',
+        });
+        const entry = await dir.getFileHandle('file.txt');
+        const writable = await entry.createWritable({keepExistingData: true});
+        await writable.write({type: 'write', position: 1, data: '1'});
+        await writable.close();
+        expect(fs.readFileSync('/file.txt', 'utf8')).toBe('.1.');
+      });
+
+      test('can seek and then write', async () => {
+        const { dir, fs } = setup({
+          'file.txt': '...',
+        });
+        const entry = await dir.getFileHandle('file.txt');
+        const writable = await entry.createWritable({keepExistingData: true});
+        await writable.write({type: 'seek', position: 1});
+        await writable.write({type: 'write', data: Buffer.from('1')});
+        expect(fs.readFileSync('/file.txt', 'utf8')).toBe('.1.');
       });
     });
   });

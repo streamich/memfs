8e0136a9fa777680ef47b60cc3cd7ae752e48e82
streamich <streamich@gmail.com>
2023-06-16 11:12:17 +0200

    feat: 🎸 add pathToLocation() utility

---
+++ src/fsa-to-node/__tests__/util.test.ts
@@ -0,0 +1,27 @@
+import { pathToLocation } from '../util';
+
+describe('pathToLocation()', () => {
+  test('handles empty string', () => {
+    expect(pathToLocation('')).toStrictEqual([[], '']);
+  });
+
+  test('no path, just filename', () => {
+    expect(pathToLocation('scary.exe')).toStrictEqual([[], 'scary.exe']);
+  });
+
+  test('multiple steps in the path', () => {
+    expect(pathToLocation('/gg/wp/hf/gl.txt')).toStrictEqual([['gg', 'wp', 'hf'], 'gl.txt']);
+    expect(pathToLocation('gg/wp/hf/gl.txt')).toStrictEqual([['gg', 'wp', 'hf'], 'gl.txt']);
+    expect(pathToLocation('/wp/hf/gl.txt')).toStrictEqual([['wp', 'hf'], 'gl.txt']);
+    expect(pathToLocation('wp/hf/gl.txt')).toStrictEqual([['wp', 'hf'], 'gl.txt']);
+    expect(pathToLocation('/hf/gl.txt')).toStrictEqual([['hf'], 'gl.txt']);
+    expect(pathToLocation('hf/gl.txt')).toStrictEqual([['hf'], 'gl.txt']);
+    expect(pathToLocation('/gl.txt')).toStrictEqual([[], 'gl.txt']);
+    expect(pathToLocation('gl.txt')).toStrictEqual([[], 'gl.txt']);
+  });
+
+  test('handles double slashes', () => {
+    expect(pathToLocation('/gg/wp//hf/gl.txt')).toStrictEqual([['gg', 'wp', '', 'hf'], 'gl.txt']);
+    expect(pathToLocation('//gl.txt')).toStrictEqual([[''], 'gl.txt']);
+  });
+});

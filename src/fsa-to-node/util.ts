cb92a9953d67aa32a8d6c6d40b1aa34be7b45c92
streamich <streamich@gmail.com>
2023-06-16 11:12:17 +0200

    feat: 🎸 add pathToLocation() utility

---
+++ src/fsa-to-node/util.ts
@@ -0,0 +1,11 @@
+import {FsaToNodeConstants} from "./constants";
+import type {FsLocation} from "./types";
+
+export const pathToLocation = (path: string): FsLocation => {
+  if (path[0] === FsaToNodeConstants.Separator) path = path.slice(1);
+  const lastSlashIndex = path.lastIndexOf(FsaToNodeConstants.Separator);
+  if (lastSlashIndex === -1) return [[], path];
+  const file = path.slice(lastSlashIndex + 1);
+  const folder = path.slice(0, lastSlashIndex).split(FsaToNodeConstants.Separator);
+  return [folder, file];
+};

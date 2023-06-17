b68ea2ad0c36602ca0888086318e0d946cd66d27
streamich <streamich@gmail.com>
2023-06-17 18:29:12 +0200

    feat: 🎸 add some common objects

---
+++ src/node/types/FsCommonObjects.ts
@@ -0,0 +1,18 @@
+import type {constants} from "../../constants";
+import type * as misc from "./misc";
+
+export interface FsCommonObjects {
+  F_OK: number;
+  R_OK: number;
+  W_OK: number;
+  X_OK: number;
+  constants: typeof constants;
+  Stats: new (...args: unknown[]) => misc.IStats;
+  StatFs: unknown,
+  Dir: unknown;
+  Dirent: new (...args: unknown[]) => misc.IDirent;
+  StatsWatcher: new (...args: unknown[]) => misc.IStatWatcher;
+  FSWatcher: new (...args: unknown[]) => misc.IFSWatcher;
+  ReadStream: new (...args: unknown[]) => misc.IReadStream;
+  WriteStream: new (...args: unknown[]) => misc.IWriteStream;
+}

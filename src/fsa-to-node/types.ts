a1a61c5dc8a2b53b6f1754b922a1965b459ca420
Vadim Dalecky <streamich@gmail.com>
2023-06-19 00:33:00 +0200

    style: 💄 run Prettier

--- src/fsa-to-node/types.ts
+++ src/fsa-to-node/types.ts
@@ -12,7 +12,7 @@ export interface FsaNodeSyncAdapterApi {
   readFile(req: { filename: string; opts?: opts.IReadFileOptions }): Uint8Array;
   writeFile(req: { filename: string; data: Uint8Array; opts?: opts.IWriteFileOptions }): void;
   appendFile(req: { filename: string; data: Uint8Array; opts?: opts.IAppendFileOptions }): void;
-  copy(req: { src: string; dst: string, flags?: number }): void;
+  copy(req: { src: string; dst: string; flags?: number }): void;
   move(req: { src: string; dst: string }): void;
   rmdir(req: [filename: string, opts?: opts.IRmdirOptions]): void;
   rm(req: [filename: string, opts?: opts.IRmOptions]): void;

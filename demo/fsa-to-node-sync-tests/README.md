4cce749c3b43e9d7b02767e36a4a23ad7d613990
streamich <streamich@gmail.com>
2023-06-19 19:50:48 +0200

    docs: ✏️ add tarball demo

---
+++ demo/fsa-to-node-sync-tests/README.md
@@ -0,0 +1,4 @@
+This demo executes tests of *synchronous* Node.js API built on top of File
+System Access API in browser.
+
+Select an empty folder and see assertions in the console.
---
+++ demo/fsa-to-node-zipfile/main.ts
@@ -0,0 +1,39 @@
+(window as any).process = require('process/browser');
+(window as any).Buffer = require('buffer').Buffer;
+
+import type * as fsa from '../../src/fsa/types';
+import {FsaNodeFs} from '../../src/fsa-to-node';
+const tar = require('tar-stream');
+
+const demo = async (dir: fsa.IFileSystemDirectoryHandle) => {
+  const fs = new FsaNodeFs(dir);
+  await fs.promises.writeFile('hello.txt', 'Hello World');
+  await fs.promises.writeFile('cool.txt', 'Cool Worlds channel');
+  
+  const list = await fs.promises.readdir('/') as string[];
+
+  const pack = tar.pack();
+  const tarball = fs.createWriteStream('backups.tar');
+  pack.pipe(tarball);
+
+  for (const item of list) {
+    if (item[0] === '.') continue;
+    const stat = await fs.promises.stat(item);
+    if (!stat.isFile()) continue;
+    pack.entry({ name: '/backups/' + item }, await fs.promises.readFile('/' + item), () => {});
+  }
+  
+  pack.finalize();
+};
+
+const main = async () => {
+  const button = document.createElement("button");
+  button.textContent = "Select an empty folder";
+  document.body.appendChild(button);
+  button.onclick = async () => {
+    const dir = await (window as any).showDirectoryPicker({id: 'demo', mode: 'readwrite'});
+    await demo(dir);
+  };
+};
+
+main();

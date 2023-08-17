45d1784021be3d35519431da32540cfff03f9e7c
SukkaW <isukkaw@gmail.com>
2023-08-17 23:11:18 +0800

    feat: add `queueMicrotask` implementation

---
+++ src/queueMicrotask.ts
@@ -0,0 +1,13 @@
+import setImmediate from './setImmediate';
+
+type TQueueMicroTask = (callback: () => void) => void;
+
+let promise: Promise<void>
+
+const _queueMicrotask: TQueueMicroTask = typeof queueMicrotask === 'function'
+  ? queueMicrotask.bind(typeof window !== 'undefined' ? window : global)
+  : cb => (promise ||= Promise.resolve())
+    .then(cb)
+    .catch(err => setImmediate(() => { throw err }))
+
+export default _queueMicrotask;

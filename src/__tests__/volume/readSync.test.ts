113f4a6c7253d1a807fb208cfbe150525e1e6ffe
uhyo <uhyo@uhy.ooo>
2023-08-20 17:06:21 +0900

    test(volume): add tests for readSync

--- src/__tests__/volume/readSync.test.ts
+++ src/__tests__/volume/readSync.test.ts
@@ -14,7 +14,29 @@ describe('.readSync(fd, buffer, offset, length, position)', () => {
     const fn = () => vol.readSync(vol.openSync('/test.txt', 'r'), buf, 0, 10, 3);
     expect(fn).toThrow('ERR_OUT_OF_RANGE');
   });
-  xit('Read over file boundary', () => {});
-  xit('Read multiple times, caret position should adjust', () => {});
+  it('Read over file boundary', () => {
+    const vol = create({ '/test.txt': '01234567' });
+    const buf = Buffer.alloc(3, 0);
+    const bytes = vol.readSync(vol.openSync('/test.txt', 'r'), buf, 0, 3, 6);
+    expect(bytes).toBe(2);
+    expect(buf.equals(Buffer.from('67\0'))).toBe(true);
+  });
+  it('Read multiple times, caret position should adjust', () => {
+    const vol = create({ '/test.txt': '01234567' });
+    const buf = Buffer.alloc(3, 0);
+    const fd = vol.openSync('/test.txt', 'r');
+    let bytes = vol.readSync(fd, buf, 0, 3, null);
+    expect(bytes).toBe(3);
+    expect(buf.equals(Buffer.from('012'))).toBe(true);
+    bytes = vol.readSync(fd, buf, 0, 3, null);
+    expect(bytes).toBe(3);
+    expect(buf.equals(Buffer.from('345'))).toBe(true);
+    bytes = vol.readSync(fd, buf, 0, 3, null);
+    expect(bytes).toBe(2);
+    expect(buf.equals(Buffer.from('675'))).toBe(true);
+    bytes = vol.readSync(fd, buf, 0, 3, null);
+    expect(bytes).toBe(0);
+    expect(buf.equals(Buffer.from('675'))).toBe(true);
+  });
   xit('Negative tests', () => {});
 });

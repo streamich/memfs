cd919f6ca28f3534d7a1a6028129b4beaf1e0452
streamich <streamich@gmail.com>
2023-10-07 13:21:32 +0200

    refactor: 💡 simplify queueMicrotask ponyfill

--- src/queueMicrotask.ts
+++ src/queueMicrotask.ts
@@ -1,17 +1,4 @@
-import setImmediate from './setImmediate';
-
-type TQueueMicroTask = (callback: () => void) => void;
-
-let promise: Promise<void>;
-
-const _queueMicrotask: TQueueMicroTask =
-  typeof queueMicrotask === 'function'
-    ? queueMicrotask.bind(typeof window !== 'undefined' ? window : global)
-    : cb =>
-        (promise ||= Promise.resolve()).then(cb).catch(err =>
-          setImmediate(() => {
-            throw err;
-          }),
-        );
-
-export default _queueMicrotask;
+export default typeof queueMicrotask === 'function' ? queueMicrotask : <typeof queueMicrotask>(cb =>
+      Promise.resolve()
+        .then(() => cb())
+        .catch(() => {}));

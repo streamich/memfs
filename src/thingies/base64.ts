dd1c5a48f10b0770172425dfcfb579787e82ecbb
Gareth Jones <jones258@gmail.com>
2024-02-18 10:32:34 +1300

    fix: remove unneeded code

--- src/thingies/base64.ts
+++
@@ -1,3 +0,0 @@
-export const encode64 = (str: string) => Buffer.from(str).toString('base64');
-
-export const decode64 = (str: string) => Buffer.from(str, 'base64').toString();
--- src/thingies/hash.ts
+++
@@ -1,6 +0,0 @@
-export const hash = (str: string): number => {
-  let hash = 5381;
-  let i = str.length;
-  while (i) hash = (hash * 33) ^ str.charCodeAt(--i);
-  return hash >>> 0;
-};

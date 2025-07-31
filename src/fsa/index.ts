5b5431b8d216009cf2b87a0421f7768953073485
Va Da <streamich@gmail.com>
2025-07-31 23:48:58 +0000

    feat: 🎸 export fsa() factory

--- src/fsa/index.ts
+++ src/fsa/index.ts
@@ -20,3 +20,18 @@ export const coreToFsa = (
 ): CoreFileSystemDirectoryHandle => {
   return new CoreFileSystemDirectoryHandle(core, dirPath, ctx);
 };
+
+/**
+ * Create a new instance of an in-memory File System Access API
+ * implementation rooted at the root directory of the filesystem.
+ *
+ * @param ctx Optional context for the File System Access API.
+ * @returns A File System Access API implementation `dir` rooted at
+ *     the root directory of the filesystem, as well as the `core`
+ *     file system itself.
+ */
+export const fsa = (ctx?: Partial<CoreFsaContext>) => {
+  const core = new Superblock();
+  const dir = new CoreFileSystemDirectoryHandle(core, '/', ctx);
+  return {dir, core};
+};

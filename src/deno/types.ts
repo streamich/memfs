aa77c5b5f267d9d374f05388bc1224896eed541c
streamich <streamich@gmail.com>
2025-09-13 16:59:38 +0200

    feat: 🎸 setup Deno types

---
+++ src/deno/types.ts
@@ -0,0 +1,47 @@
+export interface DenoFs {
+  /**
+   * Creates a new directory with the specified path.
+   *
+   * ```ts
+   * await Deno.mkdir("new_dir");
+   * await Deno.mkdir("nested/directories", { recursive: true });
+   * await Deno.mkdir("restricted_access_dir", { mode: 0o700 });
+   * ```
+   *
+   * Defaults to throwing error if the directory already exists.
+   *
+   * Requires `allow-write` permission.
+   *
+   * @tags allow-write
+   * @category File System
+   */
+  mkdir: (path: string | URL, options?: DenoMkdirOptions) => Promise<void>;
+}
+
+/**
+ * Options which can be set when using {@linkcode DenoFs.mkdir} and
+ * {@linkcode DenoFs.mkdirSync}.
+ *
+ * @category File System
+ */
+export interface DenoMkdirOptions {
+  /**
+   * If set to `true`, means that any intermediate directories will also be
+   * created (as with the shell command `mkdir -p`).
+   *
+   * Intermediate directories are created with the same permissions.
+   *
+   * When recursive is set to `true`, succeeds silently (without changing any
+   * permissions) if a directory already exists at the path, or if the path
+   * is a symlink to an existing directory.
+   *
+   * @default {false}
+   */
+  recursive?: boolean;
+
+  /**
+   * Permissions to use when creating the directory (defaults to `0o777`,
+   * before the process's umask).
+   */
+  mode?: number;
+}

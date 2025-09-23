streamich/memfs

commit da1388e2b5e752158246151dd095978754fcc941

refactor: change to named imports and re-export only necessary symbols
Co-authored-by: streamich <9773803+streamich@users.noreply.github.com>

src/core/Superblock.ts
@@ -1,4 +1,4 @@
-import * as NodePath from '../vendor/node/path';
+import { sep, relative, join, dirname, isAbsolute, basename, posix } from '../vendor/node/path';
 import { Node } from './Node';
 import { Link } from './Link';
 import { File } from './File';
@@ -15,7 +15,10 @@ import { TFileId } from './types';
 
 type TCallback<TData> = (error?: any, data?: TData) => void;
 
-const { sep, relative, join, dirname } = NodePath.posix ? NodePath.posix : NodePath;
+const pathSep = posix ? posix.sep : sep;
+const pathRelative = posix ? posix.relative : relative;
+const pathJoin = posix ? posix.join : join;
+const pathDirname = posix ? posix.dirname : dirname;
 
 const {
   O_RDONLY,
@@ -200,13 +203,13 @@ export class Superblock {
     let filename: string;
     if (stepsOrFilenameOrLink instanceof Link) {
       steps = stepsOrFilenameOrLink.steps;
-      filename = sep + steps.join(sep);
+      filename = pathSep + steps.join(pathSep);
     } else if (typeof stepsOrFilenameOrLink === 'string') {
       steps = filenameToSteps(stepsOrFilenameOrLink);
       filename = stepsOrFilenameOrLink;
     } else {
       steps = stepsOrFilenameOrLink;
-      filename = sep + steps.join(sep);
+      filename = pathSep + steps.join(pathSep);
     }
 
     let curr: Link | null = this.root;
@@ -234,9 +237,9 @@ export class Superblock {
       // Resolve symlink if we're resolving all symlinks OR if this is an intermediate path component
       // This allows lstat to traverse through symlinks in intermediate directories while not resolving the final component
       if (node.isSymlink() && (resolveSymlinks || i < steps.length - 1)) {
-        const resolvedPath = NodePath.isAbsolute(node.symlink)
+        const resolvedPath = isAbsolute(node.symlink)
           ? node.symlink
-          : join(NodePath.dirname(curr.getPath()), node.symlink); // Relative to symlink's parent
+          : pathJoin(dirname(curr.getPath()), node.symlink); // Relative to symlink's parent
 
         steps = filenameToSteps(resolvedPath).concat(steps.slice(i + 1));
         curr = this.root;
@@ -304,7 +307,7 @@ export class Superblock {
     const steps: string[] = (
       filenameOrSteps instanceof Array ? filenameOrSteps : filenameToSteps(filenameOrSteps)
     ).slice(0, -1);
-    const filename: string = sep + steps.join(sep);
+    const filename: string = pathSep + steps.join(pathSep);
     const link = this.getLinkOrThrow(filename, funcName);
     if (!link.getNode().isDirectory()) throw createError(ERROR_CODE.ENOTDIR, funcName, filename);
     return link;
@@ -345,7 +348,7 @@ export class Superblock {
       const node = child.getNode();
       if (node.isFile()) {
         let filename = child.getPath();
-        if (path) filename = relative(path, filename);
+        if (path) filename = pathRelative(path, filename);
         json[filename] = asBuffer ? node.getBuffer() : node.getString();
       } else if (node.isDirectory()) {
         this._toJSON(child, json, path, asBuffer);
@@ -354,7 +357,7 @@ export class Superblock {
 
     let dirPath = link.getPath();
 
-    if (path) dirPath = relative(path, dirPath);
+    if (path) dirPath = pathRelative(path, dirPath);
 
     if (dirPath && isEmpty) {
       json[dirPath] = null;
@@ -486,7 +489,7 @@ export class Superblock {
       // Note that this will still throw if the ENOENT came from one of the
       // intermediate directories instead of the file itself.
       if (err.code === ERROR_CODE.ENOENT && flagsNum & O_CREAT) {
-        const dirname: string = NodePath.dirname(filename);
+        const dirname: string = pathDirname(filename);
         const dirLink: Link = this.getResolvedLinkOrThrow(dirname);
         const dirNode = dirLink.getNode();
 
@@ -581,7 +584,7 @@ export class Superblock {
       if (err.code) err = createError(err.code, 'link', filename1, filename2);
       throw err;
     }
-    const dirname2 = NodePath.dirname(filename2);
+    const dirname2 = pathDirname(filename2);
     let dir2: Link;
     try {
       dir2 = this.getLinkOrThrow(dirname2, 'link');
@@ -590,7 +593,7 @@ export class Superblock {
       if (err.code) err = createError(err.code, 'link', filename1, filename2);
       throw err;
     }
-    const name = NodePath.basename(filename2);
+    const name = basename(filename2);
     if (dir2.getChild(name)) throw createError(ERROR_CODE.EEXIST, 'link', filename1, filename2);
     const node = link1.getNode();
     node.nlink++;
@@ -680,7 +683,7 @@ export class Superblock {
     oldLinkParent.deleteChild(link);
 
     // Rename should overwrite the new path, if that exists.
-    const name = NodePath.basename(newPathFilename);
+    const name = basename(newPathFilename);
     link.name = name;
     link.steps = [...newPathDirLink.steps, name];
     newPathDirLink.setChild(link.getName(), link);

src/core/json.ts
@@ -1,7 +1,7 @@
 import { Buffer } from '../internal/buffer';
-import * as pathModule from '../vendor/node/path';
+import { join, posix } from '../vendor/node/path';
 
-const { join } = pathModule.posix ? pathModule.posix : pathModule;
+const pathJoin = posix ? posix.join : join;
 
 export type DirectoryContent = string | Buffer | null;
 
@@ -18,7 +18,7 @@ export const flattenJSON = (nestedJSON: NestedDirectoryJSON): DirectoryJSON => {
     for (const path in node) {
       const contentOrNode = node[path];
       // TODO: Can we avoid using `join` here? Just concatenate?
-      const joinedPath = join(pathPrefix, path);
+      const joinedPath = pathJoin(pathPrefix, path);
       if (typeof contentOrNode === 'string' || contentOrNode instanceof Buffer) {
         flatJSON[joinedPath] = contentOrNode;
       } else if (

src/core/util.ts
@@ -1,4 +1,4 @@
-import * as pathModule from '../vendor/node/path';
+import { resolve as pathResolve, sep, posix } from '../vendor/node/path';
 import { Buffer, bufferFrom } from '../internal/buffer';
 import process from '../process';
 import { TDataOut, TEncodingExtended, ENCODING_UTF8 } from '../encoding';
@@ -6,8 +6,8 @@ import { ERRSTR } from '../node/constants';
 
 export const isWin = process.platform === 'win32';
 
-const resolveCrossPlatform = pathModule.resolve;
-const { sep } = pathModule.posix ? pathModule.posix : pathModule;
+const resolveCrossPlatform = pathResolve;
+const pathSep = posix ? posix.sep : sep;
 
 type TData = TDataOut | ArrayBufferView | DataView; // Data formats users can give us.
 
@@ -52,7 +52,7 @@ export const filenameToSteps = (filename: string, base?: string): string[] => {
   const fullPath = resolve(filename, base);
   const fullPathSansSlash = fullPath.substring(1);
   if (!fullPathSansSlash) return [];
-  return fullPathSansSlash.split(sep);
+  return fullPathSansSlash.split(pathSep);
 };
 
 export function isFd(path): boolean {

src/internal/errors.ts
@@ -5,7 +5,7 @@
 // message may change, the code should not.
 
 import * as assert from 'assert';
-import * as util from '../vendor/node/util';
+import { inspect, format } from '../vendor/node/util';
 
 const kCode = typeof Symbol === 'undefined' ? '_kCode' : (Symbol as any)('code');
 const messages = {}; // new Map();
@@ -39,8 +39,8 @@ class AssertionError extends g.Error {
       super(options.message);
     } else {
       super(
-        `${util.inspect(options.actual).slice(0, 128)} ` +
-          `${options.operator} ${util.inspect(options.expected).slice(0, 128)}`,
+        `${inspect(options.actual).slice(0, 128)} ` +
+          `${options.operator} ${inspect(options.expected).slice(0, 128)}`,
       );
     }
 
@@ -63,7 +63,7 @@ function message(key, args) {
   if (typeof msg === 'function') {
     fmt = msg;
   } else {
-    fmt = util.format;
+    fmt = format;
     if (args === undefined || args.length === 0) return msg;
     args.unshift(msg);
   }

src/node/glob.ts
@@ -1,10 +1,12 @@
-import * as pathModule from '../vendor/node/path';
+import { join, relative, resolve, posix, isAbsolute, dirname, basename } from '../vendor/node/path';
 import { toRegex } from 'glob-to-regex.js';
 import { IGlobOptions } from './types/options';
 import { pathToFilename } from './util';
 import Dirent from './Dirent';
 
-const { join, relative, resolve } = pathModule.posix;
+const pathJoin = posix.join;
+const pathRelative = posix.relative;
+const pathResolve = posix.resolve;
 
 /**
  * Check if a path matches a glob pattern
@@ -44,8 +46,8 @@ function walkDirectory(fs: any, dir: string, patterns: string[], options: IGlobO
     const entries = fs.readdirSync(dir, { withFileTypes: true }) as Dirent[];
 
     for (const entry of entries) {
-      const fullPath = join(dir, entry.name.toString());
-      const relativePath = relative(baseCwd, fullPath);
+      const fullPath = pathJoin(dir, entry.name.toString());
+      const relativePath = pathRelative(baseCwd, fullPath);
 
       // Skip if excluded
       if (isExcluded(relativePath, options.exclude)) {
@@ -76,7 +78,7 @@ function walkDirectory(fs: any, dir: string, patterns: string[], options: IGlobO
  */
 export function globSync(fs: any, pattern: string, options: IGlobOptions = {}): string[] {
   const cwd = options.cwd ? pathToFilename(options.cwd as any) : process.cwd();
-  const resolvedCwd = resolve(cwd);
+  const resolvedCwd = pathResolve(cwd);
 
   const globOptions: IGlobOptions = {
     cwd: resolvedCwd,
@@ -88,11 +90,11 @@ export function globSync(fs: any, pattern: string, options: IGlobOptions = {}): 
   let results: string[] = [];
 
   // Handle absolute patterns
-  if (pathModule.posix.isAbsolute(pattern)) {
-    const dir = pathModule.posix.dirname(pattern);
-    const basename = pathModule.posix.basename(pattern);
-    const dirResults = walkDirectory(fs, dir, [basename], { ...globOptions, cwd: dir });
-    results.push(...dirResults.map(r => pathModule.posix.resolve(dir, r)));
+  if (posix.isAbsolute(pattern)) {
+    const dir = posix.dirname(pattern);
+    const patternBasename = posix.basename(pattern);
+    const dirResults = walkDirectory(fs, dir, [patternBasename], { ...globOptions, cwd: dir });
+    results.push(...dirResults.map(r => posix.resolve(dir, r)));
   } else {
     // Handle relative patterns
     const dirResults = walkDirectory(fs, resolvedCwd, [pattern], globOptions);

src/node/volume.ts
@@ -1,4 +1,4 @@
-import * as pathModule from '../vendor/node/path';
+import { resolve, sep, relative, join, dirname, normalize, posix, isAbsolute } from '../vendor/node/path';
 import { FanOutUnsubscribe } from 'thingies/lib/fanout';
 import { Link, Superblock } from '../core';
 import Stats from './Stats';
@@ -12,7 +12,7 @@ import { constants } from '../constants';
 import { EventEmitter } from '../vendor/node/events';
 import { TEncodingExtended, TDataOut, strToEncoding, ENCODING_UTF8 } from '../encoding';
 import { FileHandle } from './FileHandle';
-import * as util from '../vendor/node/util';
+import { inherits } from '../vendor/node/util';
 import * as misc from './types/misc';
 import * as opts from './types/options';
 import { FsCallbackApi, WritevCallback } from './types/FsCallbackApi';
@@ -63,10 +63,14 @@ import { ERROR_CODE } from '../core/constants';
 import { TFileId } from '../core/types';
 import { dataToBuffer, filenameToSteps, isFd, isWin, validateFd } from '../core/util';
 
-const resolveCrossPlatform = pathModule.resolve;
+const resolveCrossPlatform = resolve;
 const { O_SYMLINK, F_OK, R_OK, W_OK, X_OK, COPYFILE_EXCL, COPYFILE_FICLONE_FORCE } = constants;
 
-const { sep, relative, join, dirname, normalize } = pathModule.posix ? pathModule.posix : pathModule;
+const pathSep = posix ? posix.sep : sep;
+const pathRelative = posix ? posix.relative : relative;
+const pathJoin = posix ? posix.join : join;
+const pathDirname = posix ? posix.dirname : dirname;
+const pathNormalize = posix ? posix.normalize : normalize;
 
 // ---------------------------------------- Types
 
@@ -607,7 +611,7 @@ export class Volume implements FsCallbackApi, FsSynchronousApi {
     // Check if trying to copy directory to subdirectory of itself
     if (srcStat.isDirectory() && this.isSrcSubdir(src, dest)) throw createError(ERROR_CODE.EINVAL, 'cp', src, dest);
     ENDURE_PARENT_DIR_EXISTS: {
-      const parent = dirname(dest);
+      const parent = pathDirname(dest);
       if (!this.existsSync(parent)) this.mkdirSync(parent, { recursive: true });
     }
     // Handle different file types
@@ -626,14 +630,14 @@ export class Volume implements FsCallbackApi, FsSynchronousApi {
 
   private isSrcSubdir(src: string, dest: string): boolean {
     try {
-      const normalizedSrc = normalize(src.startsWith('/') ? src : '/' + src);
-      const normalizedDest = normalize(dest.startsWith('/') ? dest : '/' + dest);
+      const normalizedSrc = pathNormalize(src.startsWith('/') ? src : '/' + src);
+      const normalizedDest = pathNormalize(dest.startsWith('/') ? dest : '/' + dest);
       if (normalizedSrc === normalizedDest) return true;
       // Check if dest is under src by using relative path
       // If dest is under src, the relative path from src to dest won't start with '..'
-      const relativePath = relative(normalizedSrc, normalizedDest);
+      const relativePath = pathRelative(normalizedSrc, normalizedDest);
       // If relative path is empty or doesn't start with '..', dest is under src
-      return relativePath === '' || (!relativePath.startsWith('..') && !pathModule.isAbsolute(relativePath));
+      return relativePath === '' || (!relativePath.startsWith('..') && !isAbsolute(relativePath));
     } catch (error) {
       // If path operations fail, assume it's safe (don't block the copy)
       return false;
@@ -673,8 +677,8 @@ export class Volume implements FsCallbackApi, FsSynchronousApi {
     // Read directory contents
     const entries = this.readdirSync(src) as string[];
     for (const entry of entries) {
-      const srcItem = join(src, String(entry));
-      const destItem = join(dest, String(entry));
+      const srcItem = pathJoin(src, String(entry));
+      const destItem = pathJoin(dest, String(entry));
       // Apply filter to each item
       if (options.filter && !options.filter(srcItem, destItem)) {
         continue;
@@ -692,8 +696,8 @@ export class Volume implements FsCallbackApi, FsSynchronousApi {
     options: opts.ICpOptions & { filter?: (src: string, dest: string) => boolean },
   ): void {
     let linkTarget = String(this.readlinkSync(src));
-    if (!options.verbatimSymlinks && !pathModule.isAbsolute(linkTarget))
-      linkTarget = resolveCrossPlatform(dirname(src), linkTarget);
+    if (!options.verbatimSymlinks && !isAbsolute(linkTarget))
+      linkTarget = resolveCrossPlatform(pathDirname(src), linkTarget);
     if (destStat) this.unlinkSync(dest);
     this.symlinkSync(linkTarget, dest);
   }
@@ -969,11 +973,11 @@ export class Volume implements FsCallbackApi, FsSynchronousApi {
     if (isWin) filename2 = filename2.replace(/\\/g, '/');
     return list.map(dirent => {
       if (options.recursive) {
-        let fullPath = pathModule.join(dirent.parentPath, dirent.name.toString());
+        let fullPath = pathJoin(dirent.parentPath, dirent.name.toString());
         if (isWin) {
           fullPath = fullPath.replace(/\\/g, '/');
         }
-        return fullPath.replace(filename2 + pathModule.posix.sep, '');
+        return fullPath.replace(filename2 + posix.sep, '');
       }
       return dirent.name;
     });
@@ -1600,7 +1604,7 @@ function allocNewPool(poolSize) {
   pool.used = 0;
 }
 
-util.inherits(FsReadStream, Readable);
+inherits(FsReadStream, Readable);
 exports.ReadStream = FsReadStream;
 function FsReadStream(vol, path, options) {
   if (!(this instanceof FsReadStream)) return new (FsReadStream as any)(vol, path, options);
@@ -1772,7 +1776,7 @@ export interface IWriteStream extends Writable {
   close();
 }
 
-util.inherits(FsWriteStream, Writable);
+inherits(FsWriteStream, Writable);
 exports.WriteStream = FsWriteStream;
 function FsWriteStream(vol, path, options) {
   if (!(this instanceof FsWriteStream)) return new (FsWriteStream as any)(vol, path, options);
@@ -2006,7 +2010,7 @@ export class FSWatcher extends EventEmitter {
       const filepath = link.getPath();
       const node = link.getNode();
       const onNodeChange = () => {
-        let filename = relative(this._filename, filepath);
+        let filename = pathRelative(this._filename, filepath);
         if (!filename) filename = this._getName();
         return this.emit('change', 'change', filename);
       };
@@ -2023,7 +2027,7 @@ export class FSWatcher extends EventEmitter {
 
       // when a new link added
       const onLinkChildAdd = (l: Link) => {
-        this.emit('change', 'rename', relative(this._filename, l.getPath()));
+        this.emit('change', 'rename', pathRelative(this._filename, l.getPath()));
 
         // 1. watch changes of the new link-node
         watchLinkNodeChanged(l);
@@ -2049,7 +2053,7 @@ export class FSWatcher extends EventEmitter {
         };
         removeLinkNodeListeners(l);
 
-        this.emit('change', 'rename', relative(this._filename, l.getPath()));
+        this.emit('change', 'rename', pathRelative(this._filename, l.getPath()));
       };
 
       // children nodes changed

src/vendor/node/path.ts
@@ -1,2 +1,1 @@
-import * as pathModule from 'node:path';
-export = pathModule;
+export { resolve, sep, posix, join, relative, dirname, normalize, isAbsolute, basename } from 'node:path';

src/vendor/node/util.ts
@@ -1,2 +1,1 @@
-import * as utilModule from 'node:util';
-export = utilModule;
+export { inspect, format, inherits, promisify } from 'node:util';

commit 7a51ac33fce9f14dbb7ed54eff417dc666887222

fix: Use cross-platform `dirname` instead of `posix.dirname`
`posix.dirname` was being used in some places, which could cause incorrect behavior on Windows systems where path separators are different. This change replaces it with the cross-platform `dirname()` to ensure consistent path handling.

This issue was introduced in commit da1388e2b5e752158246151dd095978754fcc941.

Closes #1193

src/core/Superblock.ts
@@ -13,27 +13,11 @@ import type { PathLike } from '../node/types/misc';
 import { ERROR_CODE } from './constants';
 import { TFileId } from './types';
 
-type TCallback<TData> = (error?: any, data?: TData) => void;
-
 const pathSep = posix ? posix.sep : sep;
 const pathRelative = posix ? posix.relative : relative;
 const pathJoin = posix ? posix.join : join;
-const pathDirname = posix ? posix.dirname : dirname;
 
-const {
-  O_RDONLY,
-  O_WRONLY,
-  O_RDWR,
-  O_CREAT,
-  O_EXCL,
-  O_TRUNC,
-  O_APPEND,
-  O_DIRECTORY,
-  O_SYMLINK,
-  F_OK,
-  COPYFILE_EXCL,
-  COPYFILE_FICLONE_FORCE,
-} = constants;
+const { O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, O_EXCL, O_TRUNC, O_APPEND, O_DIRECTORY } = constants;
 
 /**
  * Represents a filesystem superblock, which is the root of a virtual
@@ -94,8 +78,6 @@ export class Superblock {
   constructor(props = {}) {
     const root = this.createLink();
     root.setNode(this.createNode(constants.S_IFDIR | 0o777));
-
-    const self = this; // tslint:disable-line no-this-assignment
 
     root.setChild('.', root);
     root.getNode().nlink++;
@@ -487,8 +469,8 @@ export class Superblock {
       // Note that this will still throw if the ENOENT came from one of the
       // intermediate directories instead of the file itself.
       if (err.code === ERROR_CODE.ENOENT && flagsNum & O_CREAT) {
-        const dirname: string = pathDirname(filename);
-        const dirLink: Link = this.getResolvedLinkOrThrow(dirname);
+        const dirName = dirname(filename);
+        const dirLink = this.getResolvedLinkOrThrow(dirName);
         const dirNode = dirLink.getNode();
 
         // Check that the place we create the new file is actually a directory and that we are allowed to do so:
@@ -582,7 +564,7 @@ export class Superblock {
       if (err.code) err = createError(err.code, 'link', filename1, filename2);
       throw err;
     }
-    const dirname2 = pathDirname(filename2);
+    const dirname2 = dirname(filename2);
     let dir2: Link;
     try {
       dir2 = this.getLinkOrThrow(dirname2, 'link');

src/node/__tests__/volume/openSync.test.ts
@@ -1,4 +1,5 @@
 import { createFs } from '../../../__tests__/util';
+import { normalize, dirname } from '../../../vendor/node/path';
 
 describe('openSync(path, mode[, flags])', () => {
   it('should return a file descriptor', () => {
@@ -69,6 +70,18 @@ describe('openSync(path, mode[, flags])', () => {
           fs.openSync('/foo/bar', intent);
         }).toThrowError(/EACCES/);
       });
+    });
+
+    it('should open file when using native file seperators', () => {
+      const fs = createFs({});
+      // Normalize the path to match the current operating system's format.
+      // This is crucial for catching path-related issues that arise when using native paths on Windows.
+      const filePath = normalize('/foo/bar.txt');
+
+      fs.mkdirSync(dirname(filePath));
+      fs.writeFileSync(filePath, 'test');
+
+      expect(() => fs.openSync(filePath, 'r')).not.toThrow();
     });
   });
 });